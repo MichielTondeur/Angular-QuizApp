8736b96e453a88471482bd66875a0e25d13dd865
Michiel Tondeur <michiel.tondeur@gmail.com>
2020-07-30 14:05:53 +0200

    feat: add questionservice + improve file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { GameComponent } from './game/game.component';
+import { AppComponent } from './components/app/app.component';
+import { MainComponent } from './components/main/main.component';
+import { GameComponent } from './components/game/game.component';
+
+import { QuestionService } from './services/question.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    GameComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MainComponent, GameComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [QuestionService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/game/game.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../../services/question.service';
+import { Question } from '../../interfaces/question';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent implements OnInit {
+  question: Question;
+
+  constructor(questionservice: QuestionService) {
+    this.question = questionservice.getQuestion();
+  }
+
+  ngOnInit(): void {}
+}
