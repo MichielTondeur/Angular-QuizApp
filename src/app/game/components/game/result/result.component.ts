4df3f1ae5efb78af675d28745d9d8193f1aaa005
Michiel Tondeur <michiel.tondeur@gmail.com>
2020-07-30 17:18:22 +0200

    feat(result): add answer validation

--- src/app/game/components/game/result/result.component.ts
+++ src/app/game/components/game/result/result.component.ts
@@ -8,16 +8,48 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class ResultComponent implements OnInit {
   @Input() answer: string;
   @Input() userAnswer: string;
+  result: boolean;
   @Output() next = new EventEmitter();
 
   constructor() {
     this.answer = '';
     this.userAnswer = '';
+    this.result = false;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.result = this.checkAnswer(this.answer, this.userAnswer);
+  }
 
   onNextQuestion() {
     this.next.emit();
   }
+
+  trimWhiteSpace = (answer: string): string => {
+    return answer.trim();
+  };
+
+  replaceSpecialChars = (answer: string): string => {
+    return answer.replace(/[^a-zA-Z0-9]+/gi, '');
+  };
+
+  replaceAccentedChars = (answer: string): string => {
+    return answer.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
+
+  setToLowerCase = (answer: string): string => {
+    return answer.toLowerCase();
+  };
+
+  normalizeAnswer = (answer: string): string => {
+    return this.replaceSpecialChars(
+      this.replaceAccentedChars(
+        this.setToLowerCase(this.trimWhiteSpace(answer))
+      )
+    );
+  };
+
+  checkAnswer = (answer: string, inputAnswer: string): boolean => {
+    return this.normalizeAnswer(answer) === this.normalizeAnswer(inputAnswer);
+  };
 }
