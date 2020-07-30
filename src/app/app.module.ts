1c82eea5d3de33348521ba4a7b5417aabc6a85a2
Michiel Tondeur <michiel.tondeur@gmail.com>
2020-07-30 14:08:16 +0200

    refactor(app): return app component to root location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
+import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { GameComponent } from './components/game/game.component';
 
