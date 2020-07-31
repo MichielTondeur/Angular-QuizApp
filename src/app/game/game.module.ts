26a9bfcd3e38b34013274f067680288ac887d16a
Michiel Tondeur <michiel.tondeur@gmail.com>
2020-07-31 13:53:14 +0200

    refactor: change file structure

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -5,12 +5,12 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 
 import { GameComponent } from './components/game/game.component';
-import { QuestionComponent } from './components/game/question/question.component';
-import { ResultComponent } from './components/game/result/result.component';
+import { QuestionComponent } from './components/game/components/question/question.component';
+import { ResultComponent } from './components/game/components/result/result.component';
 
-import { QuestionService } from '../services/question.service';
+import { QuestionService } from './services/question.service';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [GameComponent, QuestionComponent, ResultComponent],
